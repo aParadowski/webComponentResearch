3454b17e543d9d8c33bf853a6e1643d706a254c5
Anthony Paradowski <aparadowski@alteryx.com>
2019-10-17 10:56:06 -0600

    Reorganized examples into specific parts

---
+++ examples/introExamples/part1/shadowDom/shadow.js
@@ -0,0 +1,7 @@
+const shadowRoot = document.getElementById('example').attachShadow({ mode: 'open' });
+shadowRoot.innerHTML = `<style>
+button {
+  color: tomato;
+}
+</style>
+<button id="button">This will use the CSS color tomato <slot></slot></button>`;
